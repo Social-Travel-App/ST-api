3e15973e923ea3b92c90e5ee9e45274ca9dda1bf
williamdinh <hoangdinh2411@gmail.com>
2022-10-14 22:23:34 +0200

    test

--- api/routers/auth.router.js
+++ api/routers/auth.router.js
@@ -1,9 +1,11 @@
-const express = require("express");
-const AuthController = require("../controllers/auth.controller");
+const express = require('express')
+const AuthController = require('../controllers/auth.controller')
 
-const Auth = express.Router();
+const Auth = express.Router()
 
-Auth.post("/register",AuthController.register);
+Auth.post('/register', AuthController.register)
+Auth.get('/test', (req, res) => {
+  return res.send('welcome')
+})
 
-module.exports = Auth;
-  
+module.exports = Auth
